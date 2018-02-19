1ec04b7afd09ec5399403022b69db11013d76f14
Albert Armea <albert@albertarmea.com>
2018-02-18 20:40:18 -0800

    Improve child process reliability
    This is primarily done by spawning new processes instead of forking.
    This allows each child process to have its own database connection pool.
    
    Readability is now limited to 20 seconds of runtime, after which it is
    killed. There is still more work to improve readability-scrape's
    reliability, especially around invalid CSS.

--- article-scraper.py
+++ article-scraper.py
@@ -1,14 +1,18 @@
 import json
 import multiprocessing
+import os
 import subprocess
 import random
 import time
+
+import sqlalchemy
 
 from storage import Session, Article, Source
 
 # TODO: Later, do a crawl of known news sites (maybe in a separate script)
 
 READABILITY_PATH = "/usr/bin/readability-scrape"
+READABILITY_TIMEOUT = 20 # seconds
 
 def requestWait():
     time.sleep(random.uniform(1, 5))
@@ -16,31 +20,45 @@ def requestWait():
 # Scraping is mostly I/O-bound right now, so this is fine
 SCRAPE_PROCESSES = multiprocessing.cpu_count() * 2
 
-def scrapeArticlesFromSource(source):
-    # We just forked, so reseed to try to get a unique one
-    random.seed()
+PID = os.getpid()
+def printWithPid(item):
+    print(str(PID) + ": " + str(item))
+
+
+def scrapeArticlesFromSource(hostname):
     session = Session()
     for article in session.query(Article).filter(
-            Article.source_hostname == source.hostname,
+            Article.source_hostname == hostname,
             Article.text.is_(None)).all():
+        printWithPid(article.url)
         try:
-            print(article.url)
-            readabilityProcess = subprocess.run(
+            readabilityString = subprocess.check_output(
                     [READABILITY_PATH, "--json", article.url],
-                    stdout=subprocess.PIPE)
-            readabilityOutput = json.loads(readabilityProcess.stdout)
+                    timeout=READABILITY_TIMEOUT)
+            readabilityOutput = json.loads(readabilityString)
 
-            newTitle = readabilityOutput["title"]
-            if article.title != newTitle:
-                print("Changing title to: " + newTitle)
-                article.title = newTitle
-
+            article.title = readabilityOutput["title"]
             article.text = readabilityOutput["textContent"]
 
             session.commit()
             requestWait()
+        except subprocess.CalledProcessError as e:
+            printWithPid(e)
         except Exception as e:
-            print(e)
+            printWithPid(e)
 
-with multiprocessing.Pool(SCRAPE_PROCESSES) as pool:
-    pool.map(scrapeArticlesFromSource, Session().query(Source.hostname).all())
+if __name__ == "__main__":
+    # Spawn, don't fork, so that each child gets its own database connection
+    multiprocessing.set_start_method("spawn")
+    while True:
+        with multiprocessing.Pool(SCRAPE_PROCESSES) as pool:
+            try:
+                session = Session()
+                # Pass just the hostname so it can be serialized for the child
+                pool.map(scrapeArticlesFromSource,
+                        (s.hostname for s in session.query(Source.hostname)
+                            .all()))
+            except sqlalchemy.exc.ResourceClosedError as e:
+                printWithPid(e)
+            except Exception as e:
+                printWithPid(e)
